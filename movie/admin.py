1ee5001ba11724ee6ea1a126e34d4a6db39d7f54
twitu <bhanuka.ishan@gmail.com>
2018-06-17 20:34:36 +0530

    Models, population script and boiler plate code
    ignoring database, migrations and .pyc files

---
+++ movie/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from movie.models import Movie, Actor, Director, Genre
+
+admin.site.register(Movie)
+admin.site.register(Actor)
+admin.site.register(Director)
+admin.site.register(Genre)
---
+++ movie/models.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+    release = models.CharField(max_length=20, default="N/A")
+    poster = models.URLField(max_length=100, null=True)
+
+    def __unicode__(self):
+        return "".join([self.name, " ", self.release])
+
+    class Meta:
+        ordering = ("name",)
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=30)
+    movie_name = models.ManyToManyField(Movie)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("name",)
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=30)
+    movie_name = models.ManyToManyField(Movie)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("name",)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+    movie_name = models.ManyToManyField(Movie)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("name",)
