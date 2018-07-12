faafdbf2e45042af1df804f7eb27d86194108a2c
SHIVANGI <shivangibitsp@gmail.com>
2018-07-12 17:59:16 +0000

    Added a script db.py to populate db using cli (#2)
    * Added a script db.py to populate db using cli
    
    * Added the required functionalities
    
    1.)Added the functionality of printing movie data before user confirmation
    2.)Added option for method of populating database i.e. through file or through cli .
    #Populating through file gives following error:
    "NameError: global name 'conn' is not defined". I will rectify this in sometime.
    
    * Fixed minor bugs
    
     On repetition of Actors and Directors in database it was throwing Value Error: instance is on database, value is 'none'. This was occurring because if part of the populating actors and directors was adding without saving. 
    Fixed
    
    * Removed the main 
    
    Removed main from populate_db.py and initialised the variables in get_info_and_store().
    This is the script for populating database using text file which is called in db.py.
    
    * minor change
    
    Now db.py has both the functionalities:
    1.)It prints data before confirmation from users
    2.)Gives option for method of populating database
    
    * changed name of populate_db.py to db_script.py
    
    * changed name of db.py to populate_db.py
    
    Minor changes at places:
    Replaced populate_db by db_script

--- populate_db.py
+++ populate_db.py
@@ -1,80 +1,126 @@
+import click
 import json, re, datetime, httplib, sys
-import os, django
+import os,django
+from db_script import get_info_and_store
+import db_script
 
+#add option 'name' to take input through cli
+@click.command()
+@click.option('--name',prompt='Name of movie',help='Adds movie to the database.')
 
-def get_info_and_store(name):
-    name = re.sub(r"\s+", "%20", name)
-    conn.request("GET", film.format(name), payload)
-    res = conn.getresponse()
-    data = json.loads(res.read())
+def add(name):
+		global conn
+		global movie_id
+				
+		name = re.sub(r"\s+", "%20",name)
+    		conn.request("GET", film.format(name), payload)
+    		res = conn.getresponse()
+    		data = json.loads(res.read())
 
-    if not data['total_results']:
-        print("Could not find", name)
-        f.write(name+'\n')
-        f.flush()
-    else:
-        data_1 = data['results'][0]
+		if not data['total_results']:
+        		print("Could not find", name)
+        		f.write(name+'\n')
+        		f.flush()
+    		else:
+        		data_1 = data['results'][0]
 
-        # populating movie database
-        if Movie.objects.filter(name=data_1['title']):
-            object1 = Movie.objects.filter(name=data_1['title'])[0]
-        else:
-            if data_1['poster_path']:
-                object1 = Movie(name=data_1['title'],
-                                release=data_1['release_date'],
-                                poster=str(poster_path+data_1['poster_path']))
-            else:
-                object1 = Movie(name=data_1['title'],
-                                release=data_1['release_date'])
-            object1.save()
-        print("stored:", data_1['title'])
-        movie_id = data_1['id']
+     			# populating movie database
+       			if Movie.objects.filter(name=data_1['title']):
+            			object1 = Movie.objects.filter(name=data_1['title'])[0]
+       			else:
+           			if data_1['poster_path']:
+                			object1 = Movie(name=data_1['title'],
+                                	release=data_1['release_date'],
+                                	poster=str(poster_path+data_1['poster_path']))
+            			else:
+                			object1 = Movie(name=data_1['title'],
+                                	release=data_1['release_date'])
+			movie_id = data_1['id']
+				
+			click.echo('Movie name,Release date:%s' %object1)
+		
+			# populating genre database
+       	 		for j in data_1['genre_ids']:
+            			if Genre.objects.filter(name=genre_dict[str(j)]):
+                			object4 = Genre.objects.filter(name=genre_dict[str(j)])[0]
+               			else:
+                			object4 = Genre(name=genre_dict[str(j)])
+								
+				click.echo('Genre:%s' %object4)
+				
 
-        # populating genre database
-        for j in data_1['genre_ids']:
-            if Genre.objects.filter(name=genre_dict[str(j)]):
-                object4 = Genre.objects.filter(name=genre_dict[str(j)])[0]
-                object4.movie_name.add(object1)
-            else:
-                object4 = Genre(name=genre_dict[str(j)])
-                object4.save()
-                object4.movie_name.add(object1)
+			# getting movie credits to store actors and cast
+        		conn.request("GET", credit.format(movie_id), payload)
+        		res = conn.getresponse()
+        		data = json.loads(res.read())
+			 
+			# populating director database
+        		for k in data['crew']:
+            			if k['job'] == "Director":
+                			if Director.objects.filter(name=k['name']):
+                    				object2 = Director.objects.filter(name=k['name'])[0]
+                			else:
+                    				object2 = Director(name=k['name'])
+					
+					
+			click.echo('Director:%s' %object2)
+				
+			
+			# populating actors database with only first seven or less actors
+        		if len(data['cast']) < 7:
+            			cast_range = len(data['cast'])
+        		else:
+            			cast_range = 7
 
-        # getting movie credits to store actors and cast
-        conn.request("GET", credit.format(movie_id), payload)
-        res = conn.getresponse()
-        data = json.loads(res.read())
+        		for m in range(0,cast_range):
+            			if Actor.objects.filter(name=data['cast'][m]['name']):
+                			object3 = Actor.objects.filter(name=data['cast'][m]['name'])[0]
+            			else:
+                			object3 = Actor(name=data['cast'][m]['name'])
+								
+				click.echo('Actor:%s' %object3)
 
-        # populating director database
-        for k in data['crew']:
-            if k['job'] == "Director":
-                if Director.objects.filter(name=k['name']):
-                    object2 = Director.objects.filter(name=k['name'])[0]
-                    object2.movie_name.add(object1)
-                    break
-                else:
-                    object2 = Director(name=k['name'])
-                    object2.save()
-                    object2.movie_name.add(object1)
-                    break
+			debug = click.prompt('Do you want to save the movie to database? \n(y) for "yes" and (n) for "no"')
+			if debug=='y':
+				object1.save()
+				
+				print("stored:", data_1['title'])
+				movie_id = data_1['id']
+				# populating genre database
+       	 			for j in data_1['genre_ids']:
+            				if Genre.objects.filter(name=genre_dict[str(j)]):
+                				object4 = Genre.objects.filter(name=genre_dict[str(j)])[0]
+                				object4.movie_name.add(object1)
+            				else:
+                				object4 = Genre(name=genre_dict[str(j)])
+						object4.save()	
+						object4.movie_name.add(object1)
 
-        # populating actors database with only first seven or less actors
-        if len(data['cast']) < 7:
-            cast_range = len(data['cast'])
-        else:
-            cast_range = 7
+				# populating director database
+        			for k in data['crew']:
+            				if k['job'] == "Director":
+                				if Director.objects.filter(name=k['name']):
+                    					object2 = Director.objects.filter(name=k['name'])[0]
+                    					object2.movie_name.add(object1)
+                    					break
+                				else:
+                    					object2 = Director(name=k['name'])
+							object2.save()
+							object2.movie_name.add(object1)
 
-        for m in range(0,cast_range):
-            if Actor.objects.filter(name=data['cast'][m]['name']):
-                object3 = Actor.objects.filter(name=data['cast'][m]['name'])[0]
-                object3.movie_name.add(object1)
-            else:
-                object3 = Actor(name=data['cast'][m]['name'])
-                object3.save()
-                object3.movie_name.add(object1)
-
-
-if __name__ == '__main__':
+				for m in range(0,cast_range):
+            				if Actor.objects.filter(name=data['cast'][m]['name']):
+                				object3 = Actor.objects.filter(name=data['cast'][m]['name'])[0]
+                				object3.movie_name.add(object1)
+            				else:
+                				object3 = Actor(name=data['cast'][m]['name'])
+						object3.save()
+						object3.movie_name.add(object1)        			
+               
+			else:
+				click.echo("Movie not stored in database")
+					
+if __name__=='__main__':
     # set up fixed headers, varicables and meta data
     conn = httplib.HTTPSConnection("api.themoviedb.org")
     poster_path = "https://image.tmdb.org/t/p/w640"
@@ -96,10 +142,16 @@ if __name__ == '__main__':
 
     # collect movie names in a list
     text = open(r'list.txt', 'r').readlines()
-
+   
     # file to store names that don't have data
     log = open(r'error_log.txt', 'a')
     log.write("\nScript running at" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S").__str__() + "\n")
-
-    for name in text:
-        get_info_and_store(name.rstrip())
+    
+    print('Type "f" for file and "t" for cli')
+    method=raw_input('Method for populating database(f or t):')
+	
+    if method== 't':
+    	add()
+    elif method=='f':
+	 for name in text:
+         	db_script.get_info_and_store(name.rstrip())
