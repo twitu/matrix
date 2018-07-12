43f626d655bb36f43ca316a12befa1d56634a2fa
twitu <bhanuka.ishan@gmail.com>
2018-07-13 00:19:16 +0530

    extended debug functionality to file and single mode other changes to click functionality removed redundant files fixed print string formatting

--- populate_db.py
+++ populate_db.py
@@ -1,127 +1,131 @@
 import click
-import json, re, datetime, httplib, sys
-import os,django
-from db_script import get_info_and_store
-import db_script
+import json, re, datetime, httplib
+import os, django
 
-#add option 'name' to take input through cli
-@click.command()
-@click.option('--name',prompt='Name of movie',help='Adds movie to the database.')
 
-def add(name):
-		global conn
-		global movie_id
-				
-		name = re.sub(r"\s+", "%20",name)
-    		conn.request("GET", film.format(name), payload)
-    		res = conn.getresponse()
-    		data = json.loads(res.read())
+def add(name, debug, log):
+    if debug == 'y':
+        debug = True
+    else:
+        debug = False
 
-		if not data['total_results']:
-        		print("Could not find", name)
-        		f.write(name+'\n')
-        		f.flush()
-    		else:
-        		data_1 = data['results'][0]
+    name = re.sub(r"\s+", "%20", name)
+    conn.request("GET", film.format(name), payload)
+    res = conn.getresponse()
+    data = json.loads(res.read())
 
-     			# populating movie database
-       			if Movie.objects.filter(name=data_1['title']):
-            			object1 = Movie.objects.filter(name=data_1['title'])[0]
-       			else:
-           			if data_1['poster_path']:
-                			object1 = Movie(name=data_1['title'],
-                                	release=data_1['release_date'],
-                                	poster=str(poster_path+data_1['poster_path']))
-            			else:
-                			object1 = Movie(name=data_1['title'],
-                                	release=data_1['release_date'])
-			movie_id = data_1['id']
-				
-			click.echo('Movie name,Release date:%s' %object1)
-		
-			# populating genre database
-       	 		for j in data_1['genre_ids']:
-            			if Genre.objects.filter(name=genre_dict[str(j)]):
-                			object4 = Genre.objects.filter(name=genre_dict[str(j)])[0]
-               			else:
-                			object4 = Genre(name=genre_dict[str(j)])
-								
-				click.echo('Genre:%s' %object4)
-				
+    if not data['total_results']:
+        print("Could not find ", name)
+        log.write(name + '\n')
+        log.flush()
+    else:
+        data_1 = data['results'][0]
 
-			# getting movie credits to store actors and cast
-        		conn.request("GET", credit.format(movie_id), payload)
-        		res = conn.getresponse()
-        		data = json.loads(res.read())
-			 
-			# populating director database
-        		for k in data['crew']:
-            			if k['job'] == "Director":
-                			if Director.objects.filter(name=k['name']):
-                    				object2 = Director.objects.filter(name=k['name'])[0]
-                			else:
-                    				object2 = Director(name=k['name'])
-					
-					
-			click.echo('Director:%s' %object2)
-				
-			
-			# populating actors database with only first seven or less actors
-        		if len(data['cast']) < 7:
-            			cast_range = len(data['cast'])
-        		else:
-            			cast_range = 7
+        # populating movie database
+        if Movie.objects.filter(name=data_1['title']):
+            object1 = Movie.objects.filter(name=data_1['title'])[0]
+        else:
+            if data_1['poster_path']:
+                object1 = Movie(name=data_1['title'],
+                                release=data_1['release_date'],
+                                poster=str(poster_path + data_1['poster_path']))
+            else:
+                object1 = Movie(name=data_1['title'],
+                                release=data_1['release_date'])
+        movie_id = data_1['id']
 
-        		for m in range(0,cast_range):
-            			if Actor.objects.filter(name=data['cast'][m]['name']):
-                			object3 = Actor.objects.filter(name=data['cast'][m]['name'])[0]
-            			else:
-                			object3 = Actor(name=data['cast'][m]['name'])
-								
-				click.echo('Actor:%s' %object3)
+        if debug:
+            click.echo('Movie name, Release date: %s' % object1)
 
-			debug = click.prompt('Do you want to save the movie to database? \n(y) for "yes" and (n) for "no"')
-			if debug=='y':
-				object1.save()
-				
-				print("stored:", data_1['title'])
-				movie_id = data_1['id']
-				# populating genre database
-       	 			for j in data_1['genre_ids']:
-            				if Genre.objects.filter(name=genre_dict[str(j)]):
-                				object4 = Genre.objects.filter(name=genre_dict[str(j)])[0]
-                				object4.movie_name.add(object1)
-            				else:
-                				object4 = Genre(name=genre_dict[str(j)])
-						object4.save()	
-						object4.movie_name.add(object1)
+        # populating genre database
+        for j in data_1['genre_ids']:
+            if Genre.objects.filter(name=genre_dict[str(j)]):
+                object4 = Genre.objects.filter(name=genre_dict[str(j)])[0]
+            else:
+                object4 = Genre(name=genre_dict[str(j)])
 
-				# populating director database
-        			for k in data['crew']:
-            				if k['job'] == "Director":
-                				if Director.objects.filter(name=k['name']):
-                    					object2 = Director.objects.filter(name=k['name'])[0]
-                    					object2.movie_name.add(object1)
-                    					break
-                				else:
-                    					object2 = Director(name=k['name'])
-							object2.save()
-							object2.movie_name.add(object1)
+            if debug:
+                click.echo('Genre:%s' % object4)
 
-				for m in range(0,cast_range):
-            				if Actor.objects.filter(name=data['cast'][m]['name']):
-                				object3 = Actor.objects.filter(name=data['cast'][m]['name'])[0]
-                				object3.movie_name.add(object1)
-            				else:
-                				object3 = Actor(name=data['cast'][m]['name'])
-						object3.save()
-						object3.movie_name.add(object1)        			
-               
-			else:
-				click.echo("Movie not stored in database")
-					
-if __name__=='__main__':
-    # set up fixed headers, varicables and meta data
+        # getting movie credits to store actors and cast
+        conn.request("GET", credit.format(movie_id), payload)
+        res = conn.getresponse()
+        data = json.loads(res.read())
+
+        # populating director database
+        for k in data['crew']:
+            if k['job'] == "Director":
+                if Director.objects.filter(name=k['name']):
+                    object2 = Director.objects.filter(name=k['name'])[0]
+                else:
+                    object2 = Director(name=k['name'])
+
+        if debug:
+            click.echo('Director:%s' % object2)
+
+        # populating actors database with only first seven or less actors
+        if len(data['cast']) < 7:
+            cast_range = len(data['cast'])
+        else:
+            cast_range = 7
+
+        for m in range(0, cast_range):
+            if Actor.objects.filter(name=data['cast'][m]['name']):
+                object3 = Actor.objects.filter(name=data['cast'][m]['name'])[0]
+            else:
+                object3 = Actor(name=data['cast'][m]['name'])
+
+            if debug:
+                click.echo('Actor:%s' % object3)
+
+        if debug:
+            save = ''
+            while save != 'y' and save != 'n':
+                save = click.prompt("\nDo you want to save the movie to database? \n(y) for \"yes\" and (n) for \"no\"")
+        else:
+            save = 'y'
+
+        if save == 'y':
+            object1.save()
+
+            print("stored: %s\n" % data_1['title'])
+            # populating genre database
+            for j in data_1['genre_ids']:
+                if Genre.objects.filter(name=genre_dict[str(j)]):
+                    object4 = Genre.objects.filter(name=genre_dict[str(j)])[0]
+                    object4.movie_name.add(object1)
+                else:
+                    object4 = Genre(name=genre_dict[str(j)])
+                    object4.save()
+                    object4.movie_name.add(object1)
+
+            # populating director database
+            for k in data['crew']:
+                if k['job'] == "Director":
+                    if Director.objects.filter(name=k['name']):
+                        object2 = Director.objects.filter(name=k['name'])[0]
+                        object2.movie_name.add(object1)
+                        break
+                    else:
+                        object2 = Director(name=k['name'])
+                        object2.save()
+                        object2.movie_name.add(object1)
+
+            for m in range(0, cast_range):
+                if Actor.objects.filter(name=data['cast'][m]['name']):
+                    object3 = Actor.objects.filter(name=data['cast'][m]['name'])[0]
+                    object3.movie_name.add(object1)
+                else:
+                    object3 = Actor(name=data['cast'][m]['name'])
+                    object3.save()
+                    object3.movie_name.add(object1)
+
+        else:
+            click.echo("Movie not stored in database")
+
+
+if __name__ == '__main__':
+    # set up fixed headers, variables and meta data
     conn = httplib.HTTPSConnection("api.themoviedb.org")
     poster_path = "https://image.tmdb.org/t/p/w640"
     payload = "{}"
@@ -135,23 +139,35 @@ if __name__=='__main__':
                   '27': u'Horror', '9648': u'Mystery', '10749': u'Romance', '10751': u'Family',
                   '80': u'Crime', '53': u'Thriller', '10752': u'War'}
 
-    # configuring script to understance django environment
+    # configuring script to understand django environment
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matrix.settings')
     django.setup()
     from movie.models import Movie, Actor, Director, Genre
 
-    # collect movie names in a list
-    text = open(r'list.txt', 'r').readlines()
-   
+    # ask user for single or file mode
+    mode = ''
+    while mode != 's' and mode != 'f':
+        mode = click.prompt("\nDo you want to use populate in single movie or file mode ?\n"
+                            "Please enter (s) for single or (f) for file mode")
+
+    # collect movie names is name list
+    name = []
+    if mode == 's':
+        name.append(click.prompt("\nPlease enter name of movie you want to add to database"))
+    else:
+        filename = click.prompt("\nPlease enter name of file (leave blank for default)", default="list.txt")
+        with open(filename, 'r') as batch:
+            name = batch.readlines()
+
     # file to store names that don't have data
     log = open(r'error_log.txt', 'a')
-    log.write("\nScript running at" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S").__str__() + "\n")
-    
-    print('Type "f" for file and "t" for cli')
-    method=raw_input('Method for populating database(f or t):')
-	
-    if method== 't':
-    	add()
-    elif method=='f':
-	 for name in text:
-         	db_script.get_info_and_store(name.rstrip())
+    log.write("\nScript running at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S").__str__() + "\n")
+
+    # check for debug mode
+    debug = ''
+    while debug != 'y' and debug != 'n':
+        debug = click.prompt("\nDo you want to use debug mode ?\n"
+                             "Please enter (y) for yes or (n) for no")
+
+    for movie in name:
+        add(movie.rstrip(), debug, log)
