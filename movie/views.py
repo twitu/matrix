8decff3f02440e5daee4b59b48842b58bf5b6795
SHIVANGI <shivangibitsp@gmail.com>
2018-07-17 16:14:30 +0000

    Added views for index.html and detail.html

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.http import Http404
+from . models import Movie
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    all_movies = Movie.objects.all()
+
+    return render(
+        request,
+        'movie/index.html',
+        context={'all_movies': all_movies,}
+    )
+
+def detail(request,movie_id):
+    try:
+        movie = Movie.objects.get(pk=movie_id)
+    except Movie.DoesNotExist:
+        raise Http404("Movie does not exist")
+    return render(request, 'movie/detail.html', {'movie': movie})
