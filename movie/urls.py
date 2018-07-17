950c58c3283a9117b293bb99d38aca287a63894e
SHIVANGI <shivangibitsp@gmail.com>
2018-07-17 16:12:13 +0000

    Created urls.py for movie app
    Added url mappings for movie app

---
+++ movie/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.conf.urls import url, include
+
+urlpatterns =[
+    #/music/
+    url(r'^$',views.index, name='index'),
+    #/music/21/
+    url(r'^(?P<movie_id>[0-9]+)/$',views.detail, name='detail'),
+]
