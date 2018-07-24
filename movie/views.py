c6838af53e639c8305f352680a163efd9fcd1d81
SHIVANGI <shivangibitsp@gmail.com>
2018-07-24 00:04:30 +0000

    Added view for search

--- movie/views.py
+++ movie/views.py
@@ -3,6 +3,11 @@ from __future__ import unicode_literals
 from django.http import Http404
 from . models import Movie
 from django.shortcuts import render
+from django.db.models import Q
+from django.contrib import messages
+from django.http import *
+from . models import *
+
 
 def index(request):
     all_movies = Movie.objects.all()
@@ -19,3 +24,17 @@ def detail(request,movie_id):
     except Movie.DoesNotExist:
         raise Http404("Movie does not exist")
     return render(request, 'movie/detail.html', {'movie': movie})
+
+def search(request):
+    if request.method == "POST":
+        srch = request.POST['srh']
+
+        if srch:
+            match= Movie.objects.filter(Q(name__icontains=srch)|Q(release__icontains=srch))
+            if match:
+                return render(request,'search.html', {'sr':match})
+            else:
+                messages.error(request,'no result found')
+        else:
+            return HttpResponseRedirect('/search/')
+    return render(request, 'search.html')
