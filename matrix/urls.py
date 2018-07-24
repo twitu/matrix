e2533572b75d28f2f4c2e68a981c3af67db77487
SHIVANGI <shivangibitsp@gmail.com>
2018-07-24 00:01:43 +0000

    Added url mapping for search

--- matrix/urls.py
+++ matrix/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^movie/', include("movie.urls", namespace="movie")),
+    url(r'^search/', include('haystack.urls')),
 ]
