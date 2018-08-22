5f72c773601e1e5524787b7a0ffe13ad00ee1750
Ishan Bhanuka <f2016075@pilani.bits-pilani.ac.in>
2018-08-22 10:37:33 +0530

    Revert "Added pagination function (#7)"
    This reverts commit d89d06b340ca9bbe6df588c018e3e00ec429bb5f.

--- movie/views.py
+++ movie/views.py
@@ -3,26 +3,14 @@ from __future__ import unicode_literals
 from django.http import Http404
 from . models import Movie
 from django.shortcuts import render
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def index(request):
     all_movies = Movie.objects.all()
-    paginator = Paginator(all_movies, 10)
-
-    page = request.GET.get('page', 1)
-
-    try:
-        movies = paginator.page(page)
-    except PageNotAnInteger:
-        movies = paginator.page(1)
-    except EmptyPage:
-        movies = paginator.page(paginator.num_pages)
-    # movies = paginator.get_page(page)
 
     return render(
         request,
         'movie/index.html',
-        context={'all_movies': all_movies, 'movies': movies, }
+        context={'all_movies': all_movies,}
     )
 
 def detail(request,movie_id):
