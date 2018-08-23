374a7e25c165ca1d42173e36954ac5b76b3819c0
SHIVANGI <shivangibitsp@gmail.com>
2018-08-23 14:30:50 +0000

    Fixed overflowing page tabs (#12)
    * Added pagination function
    
    * Moved css outside the loop
    
    * Fixed overflowing page tabs
    
    * Minor fixes
    
    * Modified the pagination

--- movie/views.py
+++ movie/views.py
@@ -3,27 +3,34 @@ from __future__ import unicode_literals
 from django.http import Http404
 from . models import Movie
 from django.shortcuts import render
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator, InvalidPage
 
 def index(request):
-    all_movies = Movie.objects.all()
-    paginator = Paginator(all_movies, 10)
+    movies = Movie.objects.all()
+    paginator = Paginator(movies, 10)  # Show 10 contacts per page
 
-    page = request.GET.get('page', 1)
-
+    page = request.GET.get('page')
     try:
         movies = paginator.page(page)
     except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
         movies = paginator.page(1)
     except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
         movies = paginator.page(paginator.num_pages)
-    # movies = paginator.get_page(page)
 
-    return render(
-        request,
-        'movie/index.html',
-        context={'movies': movies}
-    )
+    # Get the index of the current page
+    index = movies.number - 1
+    max_index = len(paginator.page_range)
+    start_index = index - 2 if index >= 2 else 0
+    end_index = index + 2 if index <= max_index - 2 else max_index
+
+    page_range = list(paginator.page_range)[start_index:end_index-1]
+    return render(request, 'movie/index.html', {
+        'movies': movies, 'page_range':page_range, 'page': page,
+
+    })
+
 
 def detail(request,movie_id):
     try:
