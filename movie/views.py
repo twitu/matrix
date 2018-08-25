aa5753eb73688ad8b14d1280717c5781cbf21f8a
sharifa2708 <shivangibitsp@gmail.com>
2018-08-25 16:56:31 +0000

    Fixed overflowing page tabs

--- movie/views.py
+++ movie/views.py
@@ -3,27 +3,38 @@ from __future__ import unicode_literals
 from django.http import Http404
 from . models import Movie
 from django.shortcuts import render
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator, InvalidPage
 
 def index(request):
-    all_movies = Movie.objects.all()
-    paginator = Paginator(all_movies, 10)
+    movies = Movie.objects.all()
+    paginator = Paginator(movies, 10)  # Show 25 contacts per page
 
-    page = request.GET.get('page', 1)
-
+    page = request.GET.get('page')
     try:
         movies = paginator.page(page)
     except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
         movies = paginator.page(1)
     except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
         movies = paginator.page(paginator.num_pages)
-    # movies = paginator.get_page(page)
 
-    return render(
-        request,
-        'movie/index.html',
-        context={'all_movies': all_movies, 'movies': movies, }
-    )
+        # Get the index of the current page
+    index = movies.number - 1  # edited to something easier without index
+        # This value is maximum index of your pages, so the last page - 1
+    max_index = len(paginator.page_range)
+        # You want a range of 7, so lets calculate where to slice the list
+    start_index = index - 3 if index >= 3 else 0
+    end_index = index + 3 if index <= max_index - 3 else max_index
+        # Get our new page range. In the latest versions of Django page_range returns
+        # an iterator. Thus pass it to list, to make our slice possible again.
+    page_range = list(paginator.page_range)[start_index:end_index]
+    return render(request, 'movie/index.html', {
+        'movies': movies, 'page_range':page_range,
+
+    })
+
+
 
 def detail(request,movie_id):
     try:
