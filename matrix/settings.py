6eb0fa5f9e930414a392ce41f112e066f67e0044
abhishekspeer <abhishek.abhishekgaur.gaur@gmail.com>
2018-08-27 20:14:59 +0530

    Rebuilt elasticsearch functionality

--- matrix/settings.py
+++ matrix/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'movie/templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movie',
-    'elasticsearchapp',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'matrix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- movie/models.py
+++ movie/models.py
@@ -1,6 +1,18 @@
 from __future__ import unicode_literals
-
 from django.db import models
+
+from django.utils import timezone
+from django.contrib.auth.models import User
+from .search import MovieIndex
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+
+
+
+
+
 
 
 class Movie(models.Model):
@@ -11,8 +23,25 @@ class Movie(models.Model):
     def __unicode__(self):
         return "".join([self.name, " ", self.release])
 
+    # Add indexing method
+    def indexing(self):
+        obj = MovieIndex(
+            meta = {'id: self.id'},
+            name = self.name,
+            release = self.release,
+        )
+        obj.save(index='movie-index')
+        return obj.to_dict(include_meta=True)
+
     class Meta:
         ordering = ("name",)
+
+
+@receiver(post_save, sender = Movie)
+def index_post(sender, instance, **kwargs):
+    instance.indexing()
+
+
 
 
 class Actor(models.Model):
